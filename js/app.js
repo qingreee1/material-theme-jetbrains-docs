44388d22330b590a115e38387131a30cecd51715
mallowigi <heliosaian@gmail.com>
2018-01-31 20:54:45 +0200

    new icons

--- js/app.js
+++ js/app.js
@@ -121,20 +121,6 @@ $.when($.ready).then(() => {
   jekyllApp.init();
 
   window.jekyllApp = jekyllApp;
-  //
-  //   // Back to top link
-  //   $('.backtotop').click(function () {
-  //     $('body,html').animate({
-  //       scrollTop: 0,
-  //     }, 500);
-  //     return false;
-  //   });
-  //
-  //   // Responsive Menu
-  //   $('.toggle-link').click(function () {
-  //     $('.menu').toggleClass('active');
-  //   });
-  //
 
   //
   //
